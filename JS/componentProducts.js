eae0b34158c737151eb94ef9c9f3432b397df781
ArielDeAvila <arieldeavila_1996@hotmail.com>
2022-05-02 20:54:48 -0500

    master

--- JS/componentProducts.js
+++ JS/componentProducts.js
@@ -19,9 +19,9 @@ class products extends HTMLElement{
                 <p class="description-product">${this.description}</p>
             </div>
             <div class="buy">
-                    <p>$${this.price}</p>
-                    <img src="IMG/Carrito.svg" alt="agregar al carrito" class="product-car">
-                </div>
+                <p>$${this.price}</p>
+                <img src="IMG/Carrito.svg" alt="agregar al carrito" class="product-car">
+            </div>
         </div>`
     }
 }
